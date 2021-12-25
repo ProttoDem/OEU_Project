08b3a4d2e4655f1612f2d71fdf87e63516708796
ProttoDem <79105660+ProttoDem@users.noreply.github.com>
2021-12-25 16:16:07 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ОЕУ_Проект
+{
+    class Class1
+    {
+    }
+}
